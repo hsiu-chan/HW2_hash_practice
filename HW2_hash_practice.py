b3a2600b23b19fbd927e8df552fb24856f5bc608
chc <106435999+hsiu-chan@users.noreply.github.com>
2023-03-17 23:41:28 +0800

    changed 3 files

--- HW2_hash_practice.py
+++ HW2_hash_practice.py
@@ -1,7 +1,3 @@
-# HW2_hash_practice
-# Python
-[python](HW2_hash_practice.py)
-```python 
 path='hw2_data.txt' #輸入路徑
 f = open(path) #開txt
 
@@ -15,28 +11,3 @@ for line in f.readlines():#對於每一行
 for key in dictionary:#印出結果
     print(f'{key}出現{dictionary[key]}次')
 f.close #關txt
-```
-輸出
-```
-Cheese
-出現234次
-Pizza
-出現83次
-Coke
-出現144次
-Steak
-出現46次
-Burger
-出現196次
-Fries
-出現76次
-Rib
-出現33次
-Taco
-出現57次
-Pho
-出現19次
-Potato
-出現3次
-Coke出現1次
-```
